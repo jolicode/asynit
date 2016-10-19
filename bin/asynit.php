bae1b51b2cba03b15bde9326e2a4e9403cc5f48b
Joel Wurtz <jwurtz@jolicode.com>
2016-10-19 14:33:51 +0200

    Add executable to composer

--- bin/asynit.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$loader = require __DIR__ . '/../vendor/autoload.php';
-
-\Doctrine\Common\Annotations\AnnotationRegistry::registerLoader([$loader, 'loadClass']);
-
-return (new \Asynit\Application())->run();
