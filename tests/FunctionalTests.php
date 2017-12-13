182a67df3f37c1b1d7b89c628b8345fcb6261099
Joel Wurtz <jwurtz@jolicode.com>
2017-12-13 12:03:19 +0100

    Add tests

---
+++ tests/FunctionalTests.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Asynit\Tests;
+
+use Asynit\Annotation\Depend;
+use Asynit\TestCase;
+use Psr\Http\Message\ResponseInterface;
+
+class FunctionalTests extends TestCase
+{
+    public function testReturn()
+    {
+        return 'tata';
+    }
+
+    public function testGet()
+    {
+        $response = yield $this->get('http://httpbin.org');
+
+        $this->assertInstanceOf(ResponseInterface::class, $response);
+        $this->assertStatusCode(200, $response);
+
+        return 'foo';
+    }
+
+    /**
+     * @Depend("testGet")
+     */
+    public function testDepend($value)
+    {
+        $this->assertEquals('foo', $value);
+    }
+
+    public function testStartParallel()
+    {
+        return time();
+    }
+
+    public function testParallel1()
+    {
+        yield $this->get('http://httpbin.org/delay/7');
+    }
+
+    public function testParallel2()
+    {
+        yield $this->get('http://httpbin.org/delay/7');
+    }
+
+    public function testParallel3()
+    {
+        yield $this->get('http://httpbin.org/delay/7');
+    }
+
+    public function testParallel4()
+    {
+        yield $this->get('http://httpbin.org/delay/7');
+    }
+
+    /**
+     * @Depend("testStartParallel")
+     * @Depend("testParallel1")
+     * @Depend("testParallel2")
+     * @Depend("testParallel3")
+     * @Depend("testParallel4")
+     */
+    public function testEndParallel($start)
+    {
+        $end = time();
+
+        $this->assertLessThan(10, $end - $start);
+    }
+}
---
+++ tests/HttpClientOverrideTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Asynit\Tests;
+
+use Asynit\TestCase;
+use Http\Client\Common\Plugin\BaseUriPlugin;
+use Http\Client\Common\PluginClient;
+use Http\Client\HttpAsyncClient;
+use Http\Message\UriFactory\GuzzleUriFactory;
+use Psr\Http\Message\ResponseInterface;
+
+class HttpClientOverrideTest extends TestCase
+{
+    public function setUp(HttpAsyncClient $asyncClient): HttpAsyncClient
+    {
+        $uri = (new GuzzleUriFactory())->createUri('http://httpbin.org');
+
+        return new PluginClient($asyncClient, [
+            new BaseUriPlugin($uri)
+        ]);
+    }
+
+    public function testFoo2()
+    {
+        $response = yield $this->get('/delay/3');
+
+        $this->assertInstanceOf(ResponseInterface::class, $response);
+    }
+}
