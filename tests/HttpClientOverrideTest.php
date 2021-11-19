aeb08f21036e810e6437d593ed9128dfd8af626c
Joel Wurtz <jwurtz@jolicode.com>
2021-11-19 16:46:43 +0100

    Remove useless code

--- tests/HttpClientOverrideTest.php
+++ tests/HttpClientOverrideTest.php
@@ -5,7 +5,6 @@ namespace Asynit\Tests;
 use Asynit\TestCase;
 use GuzzleHttp\Psr7\HttpFactory;
 use Http\Client\Common\Plugin\AddPathPlugin;
-use Http\Client\Common\Plugin\BaseUriPlugin;
 use Http\Client\Common\PluginClient;
 use Psr\Http\Client\ClientInterface;
 use Psr\Http\Message\ResponseInterface;
